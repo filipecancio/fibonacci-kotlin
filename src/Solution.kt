976be49c11688cb0283a5835b62788ccfc399eee
Filipe Câncio <filipe.cancio@outlook.com>
2025-11-18 22:21:10 -0300

    Implement matrix multiplication and exponentiation for Fibonacci calculation

--- src/Solution.kt
+++ src/Solution.kt
@@ -1,5 +1,4 @@
-
-fun main(){
+fun main() {
     println(Solution().fib(10))
 }
 
@@ -16,5 +15,31 @@ class Solution {
             else -> fib(n - 1) + fib(n - 2)
         }
     }
+
 }
 
+typealias Matrix = Array<IntArray>
+
+fun Matrix.multiply(b: Matrix): Matrix = arrayOf(
+    intArrayOf(
+        this[0][0] * b[0][0] + this[0][1] * b[1][0],
+        this[0][0] * b[0][1] + this[0][1] * b[1][1]
+    ),
+    intArrayOf(
+        this[1][0] * b[0][0] + this[1][1] * b[1][0],
+        this[1][0] * b[0][1] + this[1][1] * b[1][1]
+    ),
+)
+
+fun Matrix.power(n: Int): Matrix {
+    if (n == 0) return  arrayOf(
+        intArrayOf(1,0),
+        intArrayOf(0,1),
+    )
+
+    val half = this.power(n/2)
+
+    val square = half.multiply(half)
+
+    return if (n%2 == 1) this.multiply(square) else square
+}
